d108ef67e716b597571b2cfc839d6af6af358b14
Cindy Mora <cindymoraedu@gmail.com>
2021-04-07 23:07:01 -0600

    Fix error codebreaker clear variable, fix menu repetition

--- Master_Mind.py
+++ Master_Mind.py
@@ -1,7 +1,6 @@
+
 from turtle import *
 from random import *
-
-
 
 #Generacion de colores random
 colors = ["r","y","g","b","o","p"]
@@ -10,8 +9,8 @@ print()
 print("╔═  .*. ══════════════════════╗")
 print(" Colores aleatorios Generados")
 print("╚══════════════════════  .*. ═╝")
-#PENDIENTE borrar antes de enviar entregable
-print("Aleatorio",lista)
+#PROFE generaciòn de colores ocultado 
+'''print("Aleatorio",lista)'''
 print()
 
 '''
@@ -51,14 +50,9 @@ def ingresar_intento(color1,color2,color3,color4,intento_guess):
 
 #Comparacion de colores lineas en misma posicion
 def comparar(lista,codebreaker):
-    #reiniciar variable en cada intento, ya que guardaba info de siguiente funcion
-    guess = ["0","0","0","0"]
     for n in range(len(codebreaker)):
         if lista[n] == codebreaker[n]:
-            print("Coincide")
             guess[n] = "X"
-            print(guess[n])
-    print("Lista Pistas 1",guess)
 
     #Comparacion de lista random con cada linea de codebreaker
     for x in range(len(codebreaker)):
@@ -66,18 +60,22 @@ def comparar(lista,codebreaker):
             if guess[x] != "X":
                 if codebreaker[x] == lista[y]:
                     guess[x] = "-"
-                    print(guess[x])
             else:
                 break
-    print("Lista Pistas 2",guess)
+    print("Su intento es: ", codebreaker)
+    print("Pistas: ",guess)
     return guess
 
+#Borrar todos los elementos de la variable que recibe los intentos, por que no puede sobre escribir en elementos ya establecidos
+def limpiar_codebreaker(codebreaker):
+    codebreaker.clear()
 
 
 ## Menu del programa:
 running = True
 
 while running:
+    #PENDIENTE buscar como hacer que no pase por aqui en cada iteracion
     print("Menu:")
     print("1. Iniciar Juego")
     print("2. Ver instrucciones del juego")
@@ -87,32 +85,40 @@ while running:
     print("6. Salir")
     select_option = input("Seleccione una opcion: ")
     print()
+
     
     if select_option == '1':
         player = input("Introduzca el nombre: ")
         evaluation = True
         count = 0
         #Ciclo de intentos, evaluando 
-        while evaluation and count < 12:
-            print( "Opciones de color Red = r Yellow = y Green = g Blue = b Orange = o Purple = p") 
-            color1 = input("Ingrese color #1 ")
-            color2 = input("Ingrese color #2 ")
-            color3 = input("Ingrese color #3 ")
-            color4 = input("Ingrese color #4 ")
+        while evaluation:
+            if count < 12:
+                print( "Opciones de color Red = r Yellow = y Green = g Blue = b Orange = o Purple = p") 
+                color1 = input("Ingrese color #1 ")
+                color2 = input("Ingrese color #2 ")
+                color3 = input("Ingrese color #3 ")
+                color4 = input("Ingrese color #4 ")
 
-            #Se registraba lista con lista interna [[]], tuve que sustituir variable original por la misma solo con el indice cero,
-            #probé varias formas de ingresar datos a lista vacia
-            nuevo_ingreso = ingresar_intento(color1,color2,color3,color4,codebreaker)
-            codebreaker = codebreaker[0]
-            comparar(lista,codebreaker)
-            print(codebreaker)
-            count += 1
-            if guess == ["X","X","X","X"]:
-                print("Codigo acertado")                
+                #llamada a funcion Se registraba lista con lista interna [[]], tuve que sustituir variable original por la misma solo con el indice cero,
+                #probé varias formas de ingresar datos a lista vacia
+                nuevo_ingreso = ingresar_intento(color1,color2,color3,color4,codebreaker)
+                codebreaker = codebreaker[0]
+                #llamada a funcion de comparacion
+                comparar(lista,codebreaker)
+                count += 1
+                limpiar_codebreaker(codebreaker)
+                if guess == ["X","X","X","X"]:
+                    print("Codigo acertado")                
+                    evaluation = False
+                    #PENDIENTE probar si borrando afecta
+                    break
+                print("")
+
+            else:
+                print("Intentos agotados")
                 evaluation = False
-            print("")
-        print("Intentos agotados")
-        print()
+        print("")
 
     elif select_option == '2':
         print()
